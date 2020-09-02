b0d027c8d279e963180c821f40a4c25f9f7b9323
Note45 <antonion.2048@gmail.com>
2020-09-02 12:22:15 -0300

    Created sessions router and service

--- iniciando-backend-da-aplicacao/src/routes/index.ts
+++ iniciando-backend-da-aplicacao/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import appointmentsRouter from "./appointments.router";
 import userRouter from "./users.router";
+import sessionsRouter from "./sessions.router";
 
 const routes = Router();
 
 routes.use("/appointments", appointmentsRouter);
 routes.use("/users", userRouter);
+routes.use("/sessions", sessionsRouter);
 
 export default routes;
