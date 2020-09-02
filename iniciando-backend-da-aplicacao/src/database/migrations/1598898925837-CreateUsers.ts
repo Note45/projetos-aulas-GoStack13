d7bac335b8eaf643c606e02e4269e4461b2c6116
Note45 <antonion.2048@gmail.com>
2020-09-02 11:50:39 -0300

    Created user model, route and service

--- iniciando-backend-da-aplicacao/src/database/migrations/1598898925837-CreateUsers.ts
+++ iniciando-backend-da-aplicacao/src/database/migrations/1598898925837-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1598898925837 implements MigrationInterface {
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
--- iniciando-backend-da-aplicacao/src/routes/index.ts
+++ iniciando-backend-da-aplicacao/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import appointmentsRouter from "./appointments.router";
+import userRouter from "./users.router";
 
 const routes = Router();
 
 routes.use("/appointments", appointmentsRouter);
+routes.use("/users", userRouter);
 
 export default routes;
