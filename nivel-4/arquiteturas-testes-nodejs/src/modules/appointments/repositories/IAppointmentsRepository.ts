b8f215cda443cdc121733f4ea650f2be28089a0f
Note45 <antonion.2048@gmail.com>
2021-01-31 15:26:46 -0300

    Change AppointmentsRepository on appointments module

--- nivel-4/arquiteturas-testes-nodejs/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ nivel-4/arquiteturas-testes-nodejs/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
 import Appointment from "../infra/typeorm/entities/Appointment";
+import IcreateAppointmentDTO from "../dto/ICreateAppointmentDTO";
 
 export default interface AppointmentsRepository {
-  findByDate(date: Date): Promise<Appointment | undefined>
+  create(data: IcreateAppointmentDTO): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointment | undefined>;
 }
