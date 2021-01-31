746c1d3df8a3bcf470c25d0152d8d7acfd4b2f6d
Note45 <antonion.2048@gmail.com>
2021-01-31 15:44:15 -0300

    Change dependence of appoitment service

--- nivel-4/arquiteturas-testes-nodejs/src/modules/appointments/infra/http/routes/appointments.router.ts
+++ nivel-4/arquiteturas-testes-nodejs/src/modules/appointments/infra/http/routes/appointments.router.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
 import { parseISO } from "date-fns";
 
 import AppointmentsRepository from "../../typeorm/repositories/AppointmentsRepository";
@@ -8,22 +7,24 @@ import CreateAppointmentService from "../../../services/CreateAppointmentService
 import ensureAuthenticated from "../../../../users/infra/http/middlewares/ensureAuthenticated";
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get("/", async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get("/", async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentsRouter.post("/", async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const CreateAppointment = new CreateAppointmentService();
+  const CreateAppointment = new CreateAppointmentService(
+    appointmentsRepository
+  );
 
   const appointment = await CreateAppointment.execute({
     date: parsedDate,
