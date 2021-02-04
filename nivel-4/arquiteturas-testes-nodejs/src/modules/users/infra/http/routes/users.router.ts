1f4c94974e4f132ca2c8775a4960d6ea68de0217
Note45 <antonion.2048@gmail.com>
2021-02-03 21:31:48 -0300

    Create controllers to appoitments and users module

--- nivel-4/arquiteturas-testes-nodejs/src/modules/users/infra/http/routes/users.router.ts
+++ nivel-4/arquiteturas-testes-nodejs/src/modules/users/infra/http/routes/users.router.ts
@@ -1,61 +1,24 @@
 import { Router } from "express";
 import multer from "multer";
-import { container } from "tsyringe";
 import upladConfig from "../../../../../config/upload";
 
-import CreateUserService from "../../../services/CreateUserService";
-import UpdatedUserAvatarService from "../../../services/UpdatedUserAvatarService";
+import UsersController from "../controllers/UsersController";
+import UserAvatarController from "../controllers/UserAvatarController";
 
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(upladConfig);
 
-usersRouter.post("/", async (request, response) => {
-  const { name, email, password } = request.body;
-
-  const createUser = container.resolve(CreateUserService);
-
-  const user = await createUser.execute({
-    name,
-    email,
-    password,
-  });
-
-  const userWithOutPassword = {
-    id: user.id,
-    name: user.name,
-    email: user.email,
-    created_at: user.created_at,
-    updated_at: user.updated_at,
-  };
-
-  return response.json(userWithOutPassword);
-});
+usersRouter.post("/", usersController.create);
 
 usersRouter.patch(
   "/avatar",
   ensureAuthenticated,
   upload.single("avatar"),
-  async (request, response) => {
-    const updatedUserAvatar = container.resolve(UpdatedUserAvatarService);
-
-    const user = await updatedUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    const userWithOutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      avatar: user.avatar,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json(userWithOutPassword);
-  }
+  userAvatarController.update
 );
 
 export default usersRouter;
