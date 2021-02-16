57d9f3a62ed52cb5dfc99c5ee1ebd783247ff6fb
Note45 <antonion.2048@gamail.com>
2021-02-16 09:56:58 -0300

    Create test to create appointment without database dependence

--- nivel-4/arquiteturas-testes-nodejs/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ nivel-4/arquiteturas-testes-nodejs/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,23 @@
-test("sum two sumbers", () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from "../repositories/fakes/FakeAppoinementsRepository";
+import CreateAppointmentService from "./CreateAppointmentService";
+
+describe("CreateAppointment", () => {
+  it("should be able to create a new appointment", async () => {
+    const fakeAppoinementsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppoinementsRepository
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: "123",
+    });
+
+    expect(appointment).toHaveProperty("id");
+    expect(appointment.provider_id).toBe("123");
+  });
+
+  // it("should not be able to create two appointment on the same time", () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
