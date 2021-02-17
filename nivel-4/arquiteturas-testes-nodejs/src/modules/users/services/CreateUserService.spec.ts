1e9d2e8dd11c0e2b3f17c4764154cd5cea99c64f
Note45 <antonion.2048@gamail.com>
2021-02-17 09:40:53 -0300

    Create tests to CreateUserService

---
+++ nivel-4/arquiteturas-testes-nodejs/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,37 @@
+import AppError from "@shared/errors/AppError";
+import FakeUserRepository from "../repositories/fakes/FakeUserRepository";
+import CreateUserService from "./CreateUserService";
+
+describe("CreateUser", () => {
+  it("should be able to create a new user", async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    const user = await createUser.execute({
+      name: "teste aa",
+      email: "teste@teste.com",
+      password: "1231",
+    });
+
+    expect(user).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new user with same email from another", async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    await createUser.execute({
+      name: "teste aa",
+      email: "teste@teste.com",
+      password: "1231",
+    });
+
+    expect(
+      createUser.execute({
+        name: "teste aa",
+        email: "teste@teste.com",
+        password: "1231",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
