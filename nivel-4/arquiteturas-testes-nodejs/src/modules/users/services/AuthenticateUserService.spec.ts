aca729da02c921e1033bef20fb9fff40f26b3f48
Note45 <antonion.2048@gamail.com>
2021-02-20 16:23:41 -0300

    Create test to authenticate with wrong user an wrong password

--- nivel-4/arquiteturas-testes-nodejs/src/modules/users/services/AuthenticateUserService.spec.ts
+++ nivel-4/arquiteturas-testes-nodejs/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -31,4 +31,48 @@ describe("CreateUser", () => {
 
     expect(response).toHaveProperty("token");
   });
+
+  it("should not be able to authenticate with no exist user", async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: "teste@teste.com",
+        password: "1231",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate with wrong password", async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+
+    await createUser.execute({
+      name: "teste do testa",
+      email: "teste@teste.com",
+      password: "1231",
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: "teste@teste.com",
+        password: "34234",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
