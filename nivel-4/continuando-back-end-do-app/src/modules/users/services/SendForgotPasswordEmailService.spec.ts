fcc6f811e875b357f5068eac8540ef32f288914d
Note45 <antonion.2048@gamail.com>
2021-02-24 20:27:18 -0300

    Change SendForgotPasswordEmailService.spec.ts

--- nivel-4/continuando-back-end-do-app/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ nivel-4/continuando-back-end-do-app/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,20 +1,30 @@
 import AppError from "@shared/errors/AppError";
 
 import FakeUserRepository from "@modules/users/repositories/fakes/FakeUserRepository";
+import FakeUserTokensRepository from "@modules/users/repositories/fakes/FakeUserTokenRepository";
 import FakeMailProvider from "@shared/container/providers/MailProvider/fakes/FakeMailProvider";
 import SendForgotPasswordEmailService from "./SendForgotPasswordEmailService";
 
+let fakeUserRepository: FakeUserRepository;
+let fakeMailProvider: FakeMailProvider;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let sendForgotPasswordEmail: SendForgotPasswordEmailService;
+
 describe("SendForgotPasswordEmail", () => {
-  it("should be able to recovery the password using the email", async () => {
-    const fakeUserRepository = new FakeUserRepository();
-    const fakeMailProvider = new FakeMailProvider();
+  beforeEach(() => {
+    fakeUserRepository = new FakeUserRepository();
+    fakeMailProvider = new FakeMailProvider();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
 
-    const sendMail = jest.spyOn(fakeMailProvider, "sendMail");
-
-    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+    sendForgotPasswordEmail = new SendForgotPasswordEmailService(
       fakeUserRepository,
-      fakeMailProvider
+      fakeMailProvider,
+      fakeUserTokensRepository
     );
+  });
+
+  it("should be able to recovery the password using the email", async () => {
+    const sendMail = jest.spyOn(fakeMailProvider, "sendMail");
 
     await fakeUserRepository.create({
       name: "teste",
@@ -30,18 +40,26 @@ describe("SendForgotPasswordEmail", () => {
   });
 
   it("should not be able to recovery password of a not-existing user", async () => {
-    const fakeUserRepository = new FakeUserRepository();
-    const fakeMailProvider = new FakeMailProvider();
-
-    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
-      fakeUserRepository,
-      fakeMailProvider
-    );
-
     await await expect(
       sendForgotPasswordEmail.execute({
         email: "teste@teste.com",
       })
     ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should generate a forgot password token", async () => {
+    const generateToken = jest.spyOn(fakeUserTokensRepository, "generate");
+
+    const user = await fakeUserRepository.create({
+      name: "teste",
+      email: "teste@teste.com",
+      password: "123",
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: "teste@teste.com",
+    });
+
+    expect(generateToken).toHaveBeenCalledWith(user.id);
   });
 });
--- nivel-4/continuando-back-end-do-app/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ nivel-4/continuando-back-end-do-app/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -4,6 +4,7 @@ import IMailProvider from "@shared/container/providers/MailProvider/models/IMail
 import AppError from "../../../shared/errors/AppError";
 // import User from "../infra/typeorm/entities/User";
 import IUserRepository from "../repositories/IUserRepository";
+import IUserTokensRepository from "../repositories/IUserTokenRepository";
 // import IHashProvider from "../providers/HashProvider/models/IHashProvider";
 
 interface IRequest {
@@ -17,15 +18,20 @@ class SendForgotPasswordEmailService {
     private usersRepository: IUserRepository,
 
     @inject("MailProvider")
-    private mailProvider: IMailProvider
+    private mailProvider: IMailProvider,
+
+    @inject("UserTokensRepository")
+    private userTokensRepository: IUserTokensRepository
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!checkUserExists) {
+    if (!user) {
       throw new AppError("User does not exist!");
     }
+
+    await this.userTokensRepository.generate(user.id);
 
     this.mailProvider.sendMail(
       email,
