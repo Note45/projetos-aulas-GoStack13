b0938c37483a6095553bf13ea99e6becc2495191
Note45 <antonion.2048@gamail.com>
2021-03-03 09:33:07 -0300

    Create UsersTokenRepository

---
+++ nivel-4/continuando-back-end-do-app/src/modules/users/infra/typeorm/repositories/UsersTokenRepository.ts
@@ -0,0 +1,34 @@
+import { getRepository, Repository } from "typeorm";
+
+import ICreateUserDTO from "../../../dtos/ICreateUserDTO";
+import IUsersTokenRepository from "../../../repositories/IUserTokenRepository";
+
+import UserToken from "../entities/UserToken";
+
+class UsersTokenRepository implements IUsersTokenRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.ormRepository.findOne({
+      where: { token },
+    });
+
+    return userToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = await this.ormRepository.create({
+      user_id,
+    });
+
+    await this.ormRepository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export default UsersTokenRepository;
