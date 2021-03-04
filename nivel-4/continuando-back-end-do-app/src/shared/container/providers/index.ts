add44ce333286116b44b2e0f546369e2f7168d6d
Note45 <antonion.2048@gamail.com>
2021-03-04 09:21:26 -0300

    Create EtherealMailProvider

--- nivel-4/continuando-back-end-do-app/src/shared/container/providers/index.ts
+++ nivel-4/continuando-back-end-do-app/src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from "tsyringe";
 import IStorageProvide from "./StorageProvider/models/IStorageProvide";
 import DiskStorageProvider from "./StorageProvider/implementations/DiskStorageProvider";
 
+import IMailProvider from "./MailProvider/models/IMailProvider";
+import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
+
 container.registerSingleton<IStorageProvide>(
   "StorageProvider",
   DiskStorageProvider
+);
+
+container.registerSingleton<IMailProvider>(
+  "MailProvider",
+  EtherealMailProvider
 );
