e0b6027756ee0e79db0c99808cb160b74d1b822a
Note45 <antonion.2048@gamail.com>
2021-03-04 09:36:27 -0300

    Fix error not call EtherealMailProvider constructor

--- nivel-4/continuando-back-end-do-app/src/shared/container/providers/index.ts
+++ nivel-4/continuando-back-end-do-app/src/shared/container/providers/index.ts
@@ -11,7 +11,7 @@ container.registerSingleton<IStorageProvide>(
   DiskStorageProvider
 );
 
-container.registerSingleton<IMailProvider>(
+container.registerInstance<IMailProvider>(
   "MailProvider",
-  EtherealMailProvider
+  new EtherealMailProvider()
 );
