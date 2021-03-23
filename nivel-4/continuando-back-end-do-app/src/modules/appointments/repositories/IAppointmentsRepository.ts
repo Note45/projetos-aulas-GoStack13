49a41b709d2f100569cd657283f5b4c2c025a69e
Note45 <antonion.2048@gmail.com>
2021-03-23 06:17:02 -0300

    Create new method at interface AppointmentsRepository

--- nivel-4/continuando-back-end-do-app/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ nivel-4/continuando-back-end-do-app/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from "../infra/typeorm/entities/Appointment";
 import IcreateAppointmentDTO from "../dto/ICreateAppointmentDTO";
+import IFindAllInMonthFromProviderDTO from "../dto/IFindAllInMonthFromProviderDTO";
 
 export default interface AppointmentsRepository {
   create(data: IcreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findByAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO
+  ): Promise<Appointment[]>;
 }
