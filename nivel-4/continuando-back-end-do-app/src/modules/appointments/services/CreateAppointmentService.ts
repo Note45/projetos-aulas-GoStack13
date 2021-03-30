777bf589cd58abce0d308fb7c7544344f5766fc5
Note45 <antonion.2048@gmail.com>
2021-03-30 07:16:54 -0300

    Fix error on mongo conection name and add NotificationRepository to CreateAppointmentService

--- nivel-4/continuando-back-end-do-app/src/modules/appointments/services/CreateAppointmentService.ts
+++ nivel-4/continuando-back-end-do-app/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,6 +1,7 @@
-import { startOfHour, isBefore, getHours } from "date-fns";
+import { startOfHour, isBefore, getHours, format } from "date-fns";
 import { injectable, inject } from "tsyringe";
 
+import INotificationsRepository from "@modules/notifications/repositories/INotificationsRepository";
 import AppError from "../../../shared/errors/AppError";
 
 import Appointment from "../infra/typeorm/entities/Appointment";
@@ -16,7 +17,10 @@ interface IRequest {
 class CreateAppointmentService {
   constructor(
     @inject("AppointmentsRepository")
-    private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject("NotificationsRepository")
+    private notificationsRepository: INotificationsRepository
   ) {}
 
   public async execute({
@@ -52,6 +56,13 @@ class CreateAppointmentService {
       provider_id,
       user_id,
       date: appointmentDate,
+    });
+
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para dia ${dateFormatted} `,
     });
 
     return appointment;
