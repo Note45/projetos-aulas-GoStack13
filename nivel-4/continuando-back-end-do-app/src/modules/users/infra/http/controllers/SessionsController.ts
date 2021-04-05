ffbbd654326c4aaaeb9454dfa3f8989fc46ef846
Note45 <antonion.2048@gmail.com>
2021-04-05 07:10:36 -0300

    add class-transform and apply to SessionsController to format return

--- nivel-4/continuando-back-end-do-app/src/modules/users/infra/http/controllers/SessionsController.ts
+++ nivel-4/continuando-back-end-do-app/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+import { classToClass } from "class-transformer";
 
 import AuthenticateUserService from "../../../services/AuthenticateUserService";
 
@@ -14,15 +15,6 @@ export default class SessionsController {
       password,
     });
 
-    const userWithOutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      avatar: user.avatar,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json({ user: userWithOutPassword, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
