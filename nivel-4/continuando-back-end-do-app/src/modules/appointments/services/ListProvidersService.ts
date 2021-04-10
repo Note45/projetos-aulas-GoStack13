bac89dd83cdeaba3ec52ecb3a3d7c53fb2cef2a8
Note45 <antonion.2048@gmail.com>
2021-04-10 17:11:45 -0300

    setup base redis cache

--- nivel-4/continuando-back-end-do-app/src/modules/appointments/services/ListProvidersService.ts
+++ nivel-4/continuando-back-end-do-app/src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from "tsyringe";
 
 import IUserRepository from "@modules/users/repositories/IUserRepository";
 import User from "@modules/users/infra/typeorm/entities/User";
+import ICacheProvider from "@shared/container/providers/CacheProvider/models/ICacheProvider";
 
 interface IRequest {
   user_id: string;
@@ -11,13 +12,18 @@ interface IRequest {
 class ListProvidersService {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUserRepository
+    private usersRepository: IUserRepository,
+
+    @inject("CacheProvider")
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
     const users = await this.usersRepository.findAllProviders({
       except_user_id: user_id,
     });
+
+    this.cacheProvider.save("asf", "asdf");
 
     return users;
   }
