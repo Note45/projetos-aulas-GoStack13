73e2770026e42928bd6e8739e70c738c1aca22b3
Note45 <antonion.2048@gmail.com>
2021-06-02 18:29:46 -0300

    Continue dashboar

--- nivel-5/continuando-front-end-do-app/src/pages/Dashboard/index.tsx
+++ nivel-5/continuando-front-end-do-app/src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profiler } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profiler,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 import { useAuth } from '../../hooks/auth';
 
 import logoImg from '../../assets/logo.svg';
@@ -29,6 +38,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/41867280?v=4"
+                alt="Antonio Lisbôa"
+              />
+
+              <strong>Antonio Lisbôa</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
