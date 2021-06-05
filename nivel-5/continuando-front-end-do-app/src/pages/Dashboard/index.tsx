e00ae4aac725341ad9040b2eeb6939b230384b61
Note45 <antonion.2048@gmail.com>
2021-06-05 16:18:50 -0300

    Add visual appontiments styles at Dashboard page

--- nivel-5/continuando-front-end-do-app/src/pages/Dashboard/index.tsx
+++ nivel-5/continuando-front-end-do-app/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiClock, FiPower } from 'react-icons/fi';
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 import { useAuth } from '../../hooks/auth';
@@ -16,6 +18,7 @@ import { useAuth } from '../../hooks/auth';
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
 
   return (
@@ -63,6 +66,62 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/41867280?v=4"
+                  alt="Antonio Lisbôa"
+                />
+
+                <strong>Antonio Lisbôa</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/41867280?v=4"
+                  alt="Antonio Lisbôa"
+                />
+
+                <strong>Antonio Lisbôa</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/41867280?v=4"
+                  alt="Antonio Lisbôa"
+                />
+
+                <strong>Antonio Lisbôa</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
